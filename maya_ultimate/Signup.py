b94c88980f4898daed404bbe2e7d980bc0925b25
Rubin-Rodriguez <rubinrodrigues2000@gmail.com>
2021-09-16 14:25:29 +0530

    Unique key added to Signup.py

--- maya_ultimate/Signup.py
+++ maya_ultimate/Signup.py
@@ -1,16 +1,28 @@
 def signup_maya(name, phonno, email, user, passwd):
     import sqlite3 as sql
-    try:
-        with sql.connect("mayauser.db") as con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO users(name,phone_no,email,user_name,password)VALUES(?, ?, ?, ?,?)",
-                    (name, phonno, email, user, passwd))
-            con.commit()
-            msg = "Record successfully added"
-    except:
-        con.rollback()
-        msg = "Error in insert operation!"
+    with sql.connect("mayauser.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT email FROM users where email=? ", (email,))
+        account = cur.fetchall()
+        for row in account:
+            emailid = row[0]
 
-    finally:
-        con.close()
+            if emailid == email:
+                msg = "User already exists!"
+                return msg
+        try:
+            with sql.connect("mayauser.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO users(name,phone_no,email,user_name,password)VALUES(?, ?, ?, ?,?)",
+                            (name, phonno, email, user, passwd))
+                con.commit()
+                msg = "Record successfully added"
+        except Exception as e:
+            con.rollback()
+            print(e)
+            msg = "Error in insert operation!"
+
+        finally:
+            con.close()
+
     return msg
