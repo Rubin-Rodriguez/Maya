7b540856bbe82c688e1e8a177658deb5e1395642
Rubin-Rodriguez <rubinrodrigues2000@gmail.com>
2021-09-26 17:11:10 +0530

    Maya Modules

--- maya_ui/crtdb.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('mayadb.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE user(name VARCHAR(20)not null, phono VARCHAR(10) not null, email VARCHAR(50) not null,username VARCHAR(20) not null,password VARCHAR(20) not null)')
-print("Table created successfully")
-conn.close()
-
